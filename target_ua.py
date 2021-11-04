66d4f9d351599ae9c21fe413a1d57a07ed6c16a7
NaniiiGock <91615473+NaniiiGock@users.noreply.github.com>
2021-11-04 21:49:37 +0200

    adding function get_words

--- target_ua.py
+++ target_ua.py
@@ -21,14 +21,32 @@ def generate_grid() -> List[List[str]]:
 print(generate_grid())
 
 
-def get_words(f: str, letters: List[str]) -> List[str]:
-    list1 = []
-    letters += "f"
-    with open(f, "r", encoding = 'utf-8') as file:
+def get_words(file1: str, letters) -> List[str]:
+    """
+    gets words from dictionary from letters
+    >>> get_words("base.lst", "щ")
+    []
+    """
+    if len(letters) != 5:
+        return []
+    with open(file1, "r", encoding="utf-8") as file:
+        list1 = []
         for line in file:
             line = line.strip()
-            line.split("/")
-            list1.append((line[0], line[1]))
-        for i in range(30):
-            print(list1[i])
-# print(get_words("base.lst", ["a")
+            line =line.strip().split(" ")
+            if len(list(line)[0]) <6 or line[0][0] not in letters:
+                try:
+                    if (("n" and "i") in line[1]) or (("n" and "j") in line[1]):
+                        continue
+                    if ("/adj" in line) or ("adj" in line):
+                        list1.append((line[0], "adjective"))
+                    line[1] = list(line[1])
+                    if "n" in line[1]:
+                        list1.append((line[0], "noun"))
+                    if "v" in line[1]:
+                        list1.append((line[0],"verb"))
+                    if ("a" and "d" and "v") in line[1]:
+                        list1.append((line[0], "adverb"))
+                except IndexError:
+                    continue
+    return list1
